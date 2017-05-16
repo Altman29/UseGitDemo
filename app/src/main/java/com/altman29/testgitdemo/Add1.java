64d1f8e21e32db6f64b135200b8a30964be359ca
altman29 <s1yuan_chen@163.com>
2017-05-16 20:59:10 +0800

    添加一个Add1类

---
+++ app/src/main/java/com/altman29/testgitdemo/Add1.java
@@ -0,0 +1,10 @@
+package com.altman29.testgitdemo;
+
+/**
+ * Created by altman29 on 2017/5/16.
+ * e-mial:s1yuan_chen@163.com
+ */
+
+public class Add1 {
+    public static final String TAG = "Add1";
+}
